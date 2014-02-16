13b4548a715475fa3ec4b443726701415c90b5be
Mark Dessain <mark.dessain@gmail.com>
2014-02-16 18:36:36 +0000

    Adding in caculator

--- androidtest/src/main/java/com/example/androidtest/MainActivity.java
+++ androidtest/src/main/java/com/example/androidtest/MainActivity.java
@@ -1,34 +1,57 @@
 package com.example.androidtest;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+
+    private EditText numberOne;
+    private EditText numberTwo;
+    private Button addButton;
+    private TextView total;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-    }
+        numberOne = (EditText) findViewById(R.id.numberOne);
+        numberTwo = (EditText) findViewById(R.id.numberTwo);
+        addButton = (Button) findViewById(R.id.addButton);
+        total = (TextView) findViewById(R.id.total);
 
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String firstNumberResult = numberOne.getText().toString();
+                String secondNumberResult = numberTwo.getText().toString();
+
+                if (firstNumberResult.equalsIgnoreCase("")) {
+                    numberOne.setText("0");
+                    firstNumberResult = "0";
+                }
+
+                if (secondNumberResult.equalsIgnoreCase("")) {
+                    numberTwo.setText("0");
+                    secondNumberResult = "0";
+                }
+
+                Integer totalResult = Integer.parseInt(firstNumberResult) +
+                        Integer.parseInt(secondNumberResult);
+                total.setText("Total = " + Integer.toString(totalResult));
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
@@ -46,20 +69,20 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
+    public EditText getNumberOne() {
+        return numberOne;
+    }
 
-        public PlaceholderFragment() {
-        }
+    public EditText getNumberTwo() {
+        return numberTwo;
+    }
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
+    public Button getCalculationButton() {
+        return addButton;
+    }
+
+    public TextView getTotal() {
+        return total;
     }
 
 }
--- androidtest/src/test/java/DummyTest.java
+++ androidtest/src/test/java/DummyTest.java
@@ -1,24 +1,73 @@
+
+import android.widget.Button;
+
+
+import com.example.androidtest.MainActivity;
+
 import junit.framework.Assert;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import static junit.framework.Assert.assertEquals;
+
 
 @RunWith(RobolectricTestRunner.class)
 public class DummyTest {
-    @Before
-    public void setup() {
-        //do whatever is necessary before every test
-    }
 
-//    @Test
-//    public void testShouldFail() {
-//        Assert.assertTrue(false);
-//    }
+    private MainActivity mainActivity;
+
+    @Before
+    public void setUp() throws Exception {
+        mainActivity = Robolectric.buildActivity(MainActivity.class).create().get();
+    }
 
     @Test
-    public void testShouldPass() {
-        Assert.assertTrue(true);
+    public void pressingTheButtonShouldStartTheListActivity() throws Exception {
+        mainActivity.getNumberOne().setText("1");
+        mainActivity.getNumberTwo().setText("2");
+
+        mainActivity.getCalculationButton().performClick();
+
+        assertEquals(mainActivity.getTotal().getText().toString(), "Total = 3");
+    }
+
+    @Test
+    public void testMissingFirstInput() throws Exception {
+        mainActivity.getNumberOne().setText("");
+        mainActivity.getNumberTwo().setText("2");
+
+        mainActivity.getCalculationButton().performClick();
+
+        assertEquals(mainActivity.getTotal().getText().toString(), "Total = 2");
+        assertEquals(mainActivity.getNumberOne().getText().toString(), "0");
+    }
+
+    @Test
+    public void testMissingSecondInput() throws Exception {
+        mainActivity.getNumberOne().setText("1");
+        mainActivity.getNumberTwo().setText("");
+
+        mainActivity.getCalculationButton().performClick();
+
+        assertEquals(mainActivity.getTotal().getText().toString(), "Total = 1");
+        assertEquals(mainActivity.getNumberTwo().getText().toString(), "0");
+    }
+
+    @Test
+    public void testMissingBothInputs() throws Exception {
+        mainActivity.getNumberOne().setText("");
+        mainActivity.getNumberTwo().setText("");
+
+        mainActivity.getCalculationButton().performClick();
+
+        assertEquals(mainActivity.getTotal().getText().toString(), "Total = 0");
+        assertEquals(mainActivity.getNumberOne().getText().toString(), "0");
+        assertEquals(mainActivity.getNumberTwo().getText().toString(), "0");
     }
 }
+
